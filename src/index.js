f41f2ead76e023ba6919a0adee809f3bc5de7d9f
McQueen <m.gorelenko@mail.ru>
2023-01-17 19:05:10 +0300

    fix: files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  
+  let newBracketsConfig = new Map(bracketsConfig);
+
+  str.split('').forEach((elem) => {
+    if (elem == newBracketsConfig.get(stack[stack.length - 1])) {
+      stack.pop();
+    } else {
+      stack.push(elem);
+    }
+  })
+  return stack.length !== 0 ? false : true;
 }
